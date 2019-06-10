a366f6db5e24f34878c54bb715603368c261327e
Ilias Dimopoulos <idimopoulos@hotmail.com>
2019-06-10 18:23:27 +0300

    Update the images to the cest2019 flavor.

--- app/src/cest2019/java/com/ls/sponsors/GoldSponsors.java
+++ app/src/cest2019/java/com/ls/sponsors/GoldSponsors.java
@@ -12,8 +12,10 @@ public class GoldSponsors {
 
     public static List<SponsorItem> getSponsorsList(Context context) {
         List<SponsorItem> sponsors = new ArrayList<>();
-        sponsors.add(new SponsorItem(context.getString(R.string.sponsor_1), R.drawable.event_details_header));
-        sponsors.add(new SponsorItem(context.getString(R.string.sponsor_2), R.drawable.event_details_sponsors_header_2));
+
+        sponsors.add(new SponsorItem(context.getString(R.string.sponsor_1), R.drawable.sponsor_pl_psipw));
+        sponsors.add(new SponsorItem(context.getString(R.string.sponsor_2), R.drawable.sponsor_sil_aecom));
+        sponsors.add(new SponsorItem(context.getString(R.string.sponsor_3), R.drawable.sponsor_sil_aget));
         return sponsors;
     }
 
